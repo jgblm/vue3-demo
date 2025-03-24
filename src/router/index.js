f0e0e9525464c2bfb84a6e61bc695724b1fbb11a
zhaoxuliang <826946484@qq.com>
2025-03-24 17:24:56 +0800

    右上菜单栏样式调整

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,11 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-import Home from '../components/Home.vue'
+import Home from '../views/Home.vue'
 
 const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home,
+    component: ()=>import('../views/Home.vue'),
     meta: {
       title: '首页'
     },
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,24 +1,85 @@
+<script setup>
+import {ElIcon} from 'element-plus';
+import {Bell, Fold} from '@element-plus/icons-vue';
+
+const userName = 'Donna Lewis'; // 用户名示例数据
+</script>
+
 <template>
-  <div>
-    <h1>Home Page</h1>
-    {{ content }}
-    <hr>
-    <button @click="changeContent">Click Me</button>
+  <div class="basic-layout">
+    <div class="nav-side">
+      <!-- 左侧内容 -->
+    </div>
+    <div class="content-right">
+      <div class="nav-top">
+        <div class="left-section">
+          <el-icon :size="20" @click="toggleMenu">
+            <Fold />
+          </el-icon>
+          <span>面包屑</span>
+        </div>
+        <div class="right-section">
+          <el-icon :size="20">
+            <Bell/>
+          </el-icon>
+          <span>{{ userName }}</span>
+        </div>
+      </div>
+      <div class="wrapper">
+        <router-view></router-view>
+      </div>
+    </div>
   </div>
 </template>
 
-<script setup>
-import {reactive, ref, watch} from "vue";
-
-let content = reactive({
-  name: 'Home Page',
-  value: 'Hello World'
-})
-const changeContent = () => {
-  content.name='12345'
+<style scoped lang="scss">
+.basic-layout {
+  display: flex;
 }
 
-watch(content, (newValue, oldValue) => {
-  console.log(newValue, oldValue)
-})
-</script>
+.nav-side {
+  width: 200px; /* 固定左侧宽度 */
+  height: 100vh;
+  background-color: #001529; /* 示例背景色 */
+}
+
+.content-right {
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+}
+
+.nav-top {
+  height: 50px; /* 固定顶部高度 */
+  background-color: #d0d0d0; /* 示例背景色 */
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 20px;
+
+  .left-section {
+    display: flex;
+    align-items: center;
+    gap: 10px;
+  }
+
+  .right-section {
+    display: flex;
+    align-items: center;
+    gap: 10px;
+  }
+
+  .user-avatar {
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+  }
+}
+
+.wrapper {
+  flex: 1;
+  background-color: #ffffff; /* 示例背景色 */
+  padding: 20px;
+  height: calc(100vh - 50px);
+}
+</style>
